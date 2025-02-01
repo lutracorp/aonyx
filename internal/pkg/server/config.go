ee3edba408dc8d87431258c1f954dbaf87fea510
kasefuchs <kasefuchs@protonmail.com>
2025-02-01 14:38:58 +0300

    :construction: Add database package

---
+++ internal/pkg/server/config.go
@@ -0,0 +1,7 @@
+package server
+
+// Config specifies server configuration.
+type Config struct {
+	Address string // Address specifies address to use.
+	Port    uint16 // Port specifies port to use.
+}
---
+++ internal/pkg/server/server.go
@@ -0,0 +1,29 @@
+package server
+
+import (
+	"fmt"
+
+	"github.com/goccy/go-json"
+	"github.com/gofiber/fiber/v2"
+)
+
+// App stores Fiber instance.
+var App = fiber.New(
+	fiber.Config{
+		AppName:     "LutraAuth",
+		JSONEncoder: json.Marshal,
+		JSONDecoder: json.Unmarshal,
+	},
+)
+
+// Open starts the server.
+func Open(config *Config) error {
+	addr := fmt.Sprintf("%s:%d", config.Address, config.Port)
+
+	return App.Listen(addr)
+}
+
+// Close stops the server.
+func Close() error {
+	return App.Shutdown()
+}
